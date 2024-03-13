8c4ea3aaa464197d2b79d09b4147290dce53c890
yulinling <rsgltzyd@hotmail.com>
2024-03-13 18:50:31 +0800

    test LocalStronger

--- test/hlk/main.js
+++ test/hlk/main.js
@@ -1,40 +1,40 @@
 /* 解析json文件内容，执行设置工具 */
 async function setData() {
     let url = "./index.json"
-    const request = new Request(url)
+    const request = new Request(url);
 
-    const response = await fetch(request)
-    const all = await response.json()
-    setTools(all)
+    const response = await fetch(request);
+    const all = await response.json();
+    setTools(all);
 }
 
 /* 根据获取的对象，设置ul-li */
 function setTools(obj) {
     if (!obj) {
-        return 'obj is null'
+        return 'obj is null';
     }
-    const keys = Object.keys(obj)
+    const keys = Object.keys(obj);
     keys.forEach(key => {
-        const todoList = document.querySelector(`#${key}`).querySelector('ul')
+        const todoList = document.querySelector(`#${key}`).querySelector('ul');
         obj[key].forEach(element => {
-            let liChild = document.createElement('li')
+            let liChild = document.createElement('li');
             if (element.images) {
-                let imgChild = document.createElement('img')
-                imgChild.classList.add('imgs')
-                imgChild.alt = "--"
-                imgChild.src = element.images
-                liChild.appendChild(imgChild)
+                let imgChild = document.createElement('img');
+                imgChild.classList.add('imgs');
+                imgChild.alt = "--";
+                imgChild.src = element.images;
+                liChild.appendChild(imgChild);
             }
 
-            let aChild = document.createElement('a')
-            aChild.classList.add('tools')
-            aChild.textContent = element.name
-            aChild.href = element.src
+            let aChild = document.createElement('a');
+            aChild.classList.add('tools');
+            aChild.textContent = element.name;
+            aChild.href = element.src;
 
-            liChild.appendChild(aChild)
-            todoList.appendChild(liChild)
+            liChild.appendChild(aChild);
+            todoList.appendChild(liChild);
         });
     });
 }
 
-setData()
+setData();
