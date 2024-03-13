1e3283cf22ec3608279f985fc73f718a8a704683
yulinling <rsgltzyd@hotmail.com>
2024-03-13 16:56:43 +0800

    更改为felx布局和grid布局

--- test/hlk/main.js
+++ test/hlk/main.js
@@ -4,35 +4,34 @@ async function setData() {
 
     const response = await fetch(request)
     const all = await response.json()
-    setTools(Object.keys(all), all)
+    setTools(all)
 }
 
-function setTools(keys, obj) {
+function setTools(obj) {
+    if (!obj) {
+        return 'obj is null'
+    }
+    const keys = Object.keys(obj)
     keys.forEach(key => {
         const todoList = document.querySelector(`#${key}`).querySelector('ul')
-        console.log(key, obj[key])
-        if (key) {
-            obj[key].forEach(element => {
-                let liChild = document.createElement('li')
-                if (element.images) {
-                    let imgChild = document.createElement('img')
-                    imgChild.classList.add('imgs')
-                    imgChild.alt = "--"
-                    imgChild.src = element.images
-                    liChild.appendChild(imgChild)
-                }
+        obj[key].forEach(element => {
+            let liChild = document.createElement('li')
+            if (element.images) {
+                let imgChild = document.createElement('img')
+                imgChild.classList.add('imgs')
+                imgChild.alt = "--"
+                imgChild.src = element.images
+                liChild.appendChild(imgChild)
+            }
 
-                let aChild = document.createElement('a')
-                aChild.classList.add('tools')
-                aChild.textContent = element.name
-                aChild.href = element.src
+            let aChild = document.createElement('a')
+            aChild.classList.add('tools')
+            aChild.textContent = element.name
+            aChild.href = element.src
 
-                liChild.appendChild(aChild)
-                todoList.appendChild(liChild)
-            });
-        } else {
-            console.log('---')
-        }
+            liChild.appendChild(aChild)
+            todoList.appendChild(liChild)
+        });
     });
 }
 
