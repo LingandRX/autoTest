057920ff3a7e0bcde2be8bbe37a6c3120983b2e5
yulinling <rsgltzyd@hotmail.com>
2024-03-25 18:22:24 +0800

    添加计算逻辑

--- test/calculator/main.js
+++ test/calculator/main.js
@@ -1,63 +1,61 @@
 let globalNumber = '';
+let prevNumber = 0;
+let nextNumber = 0;
+let result = 0;
 
 /**
  * 为计算器的按键绑定监听事件
  */
 function setKeyEvent() {
     const keyLists = document.querySelectorAll('.key');
-    const resultPreview = document.querySelector('.result-preview');
     console.log(keyLists);
 
     keyLists.forEach((key) => {
         key.addEventListener('click', function () {
-            displayKeyCharacter(resultPreview, checkNumber(key.textContent));
             console.log(key.textContent);
+            checkNumber(key);
         })
     });
 }
 
 /**
- * 
- * @param {string} character 
- * @returns 返回检查字符结果
- */
-function checkNumber(character) {
-    let tmp = '11';
-    if (!character) {
-        throw Error('不是数字');
+ * @param {Element} key 
+*/
+function checkNumber(key) {
+    const resultPreview = document.querySelector('.result-preview');
+    if (key.className.split(' ')[1] === 'number') {
+        globalNumber += key.textContent;
+        displayKeyCharacter(resultPreview, globalNumber);
+        console.log('is number');
+    } else if (key.className.split(' ')[1] === 'decimal') {
+        console.log('is decimal');
+    } else if (key.className.split(' ')[1] === 'equal') {
+        console.log('is equal');
+        // 执行加法
+        prevNumber = globalNumber.split('+')[0];
+        nextNumber = globalNumber.split('+')[1];
+        console.log(prevNumber, nextNumber);
+        prevNumber = count(prevNumber, nextNumber, '+');
+        globalNumber = prevNumber;
+        displayKeyCharacter(resultPreview, globalNumber);
+    } else if (key.className.split(' ')[1] === 'add') {
+        globalNumber += key.textContent;
+        displayKeyCharacter(resultPreview, globalNumber);
+        console.log('is add');
+    } else if (key.className.split(' ')[1] === 'clean' || key.className.split(' ')[1] === 'clean-error') {
+        console.log('clean');
+        globalNumber = 0;
+        prevNumber = 0;
+        nextNumber = 0;
+        displayKeyCharacter(resultPreview, globalNumber);
+    } else {
+        console.log('not number');
     }
-    if (typeof str === 'number') {
-        character = String(character);
-    }
-    // 检查按键，返回功能str
-    switch (character) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            tmp = character;
-            break;
-        case '÷':
-        case 'x':
-        case '-':
-        case '+':
-            tmp = 'calculator';
-            break;
-        default:
-            return '暂不支持';
-    }
-    return tmp;
 }
 
 /**
  * 在结果框中显示按键的结果
- * @param {Element} element 
+ * @param {Element} element
  * @param {String} character
  * @returns 
  */
@@ -65,8 +63,13 @@ function displayKeyCharacter(element, character) {
     if (!element && !character) {
         return '结果为空';
     }
-    globalNumber += character;
-    element.textContent = globalNumber;
+    if (character === 'ERROR') {
+        globalNumber = '';
+        element.textContent = 'ERROR';;
+        return;
+    }
+
+    element.textContent = character;
 }
 /**
  * 异步计算函数
@@ -75,8 +78,13 @@ function displayKeyCharacter(element, character) {
  * @param {string} symbo - 计算符号
  * @return {Promise} 返回计算结果
  */
-async function count(prev, next, symbol) {
+function count(prev, next, symbol) {
     let tmp;
+    if (!prev || !next) { 
+        return prev || next;
+    }
+    prev = Number(prev);
+    next = Number(next);
     switch (symbol) {
         case 'x':
             tmp = prev * next;
@@ -91,14 +99,10 @@ async function count(prev, next, symbol) {
             tmp = prev - next;
             break;
         default:
-            return Promise.reject('Error');
+            return 'Error';
     }
-
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(tmp);
-        }, 2000);
-    });
+    console.log(tmp);
+    return tmp;
 }
 
 const test = document.querySelector('.test');
@@ -112,3 +116,4 @@ test.addEventListener('click', async () => {
 });
 
 setKeyEvent();
+console.log(count('', 3, '+'));
