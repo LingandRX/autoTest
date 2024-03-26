32492268f5de66c8de3d78cb54086ab9b9679978
yulinling <rsgltzyd@hotmail.com>
2024-03-26 18:52:53 +0800

    将计算的数值抽象为一个类，重新编写计算函数

--- test/calculator/main.js
+++ test/calculator/main.js
@@ -3,6 +3,69 @@ let prevNumber = 0;
 let nextNumber = 0;
 let result = 0;
 
+const myNumber = (function () {
+    // 使用Symbol生成唯一指定标识
+    let _globalNumber = Symbol('globalNumber');
+    let _prevNumber = Symbol('prevNumber');
+    let _nextNumber = Symbol('nextNumber');
+    let _result = Symbol('result');
+    let _symbol = Symbol('symbol');
+
+    class myNumber {
+        constructor() {
+            this[_globalNumber] = '';
+            this[_prevNumber] = 0;
+            this[_nextNumber] = 0;
+            this[_result] = 0;
+            this[_symbol] = '';
+        }
+
+        getGlobal() {
+            return this[_globalNumber];
+        }
+
+        setGlobal(newGlobal = 'none') {
+            this[_globalNumber] = newGlobal;
+        }
+
+        getPrev() {
+            return this[_prevNumber];
+        }
+
+        setPrev(newPrev = 'none') {
+            this[_prevNumber] = newPrev;
+        }
+
+        getNext() {
+            return this[_nextNumber];
+        }
+
+        setNext(newNext = 'none') {
+            this[_nextNumber] = newNext;
+        }
+
+        getResult() {
+            return this[_result];
+        }
+
+        setResult(newResult = 'none') {
+            this[_result] = newResult;
+        }
+
+        getSymbol() {
+            return this[_symbol];
+        }
+
+        setSymbol(newSymbol = 'none') {
+            this[_symbol] = newSymbol;
+        }
+    }
+
+    return myNumber;
+})();
+
+
+let e = new myNumber();
 /**
  * 为计算器的按键绑定监听事件
  */
@@ -19,35 +82,25 @@ function setKeyEvent() {
 }
 
 /**
+ * 根据按键触发功能
  * @param {Element} key 
 */
 function checkNumber(key) {
     const resultPreview = document.querySelector('.result-preview');
     if (key.className.split(' ')[1] === 'number') {
-        globalNumber += key.textContent;
-        displayKeyCharacter(resultPreview, globalNumber);
-        console.log('is number');
+        enterNumber(key, resultPreview);
     } else if (key.className.split(' ')[1] === 'decimal') {
         console.log('is decimal');
     } else if (key.className.split(' ')[1] === 'equal') {
-        console.log('is equal');
-        // 执行加法
-        prevNumber = globalNumber.split('+')[0];
-        nextNumber = globalNumber.split('+')[1];
-        console.log(prevNumber, nextNumber);
-        prevNumber = count(prevNumber, nextNumber, '+');
-        globalNumber = prevNumber;
-        displayKeyCharacter(resultPreview, globalNumber);
+        equalNumber(key, resultPreview);
     } else if (key.className.split(' ')[1] === 'add') {
-        globalNumber += key.textContent;
-        displayKeyCharacter(resultPreview, globalNumber);
-        console.log('is add');
+        addNumber(key, resultPreview);
+    } else if (key.className.split(' ')[1] === 'division') {
+        divisionNumber(key, resultPreview);
+    } else if (key.className.split(' ')[1] === 'subtract') {
+        subtractNumber(key, resultPreview);
     } else if (key.className.split(' ')[1] === 'clean' || key.className.split(' ')[1] === 'clean-error') {
-        console.log('clean');
-        globalNumber = 0;
-        prevNumber = 0;
-        nextNumber = 0;
-        displayKeyCharacter(resultPreview, globalNumber);
+        cleanNumber(key, resultPreview);
     } else {
         console.log('not number');
     }
@@ -80,7 +133,7 @@ function displayKeyCharacter(element, character) {
  */
 function count(prev, next, symbol) {
     let tmp;
-    if (!prev || !next) { 
+    if (!prev || !next) {
         return prev || next;
     }
     prev = Number(prev);
@@ -92,7 +145,7 @@ function count(prev, next, symbol) {
         case "/":
             tmp = prev * next;
             break;
-        case '+':
+        case 'add':
             tmp = prev + next;
             break;
         case '-':
@@ -105,7 +158,7 @@ function count(prev, next, symbol) {
     return tmp;
 }
 
-const test = document.querySelector('.test');
+/* const test = document.querySelector('.test');
 test.addEventListener('click', async () => {
     try {
         let tmp = await count(1, 2, '+');
@@ -113,7 +166,53 @@ test.addEventListener('click', async () => {
     } catch (error) {
         console.log(error);
     }
-});
+}); */
 
 setKeyEvent();
-console.log(count('', 3, '+'));
+
+function enterNumber(key, resultPreview) {
+    e.setGlobal(e.getGlobal() + key.textContent);
+    console.log(`e.global = ${e.getGlobal()}`);
+    displayKeyCharacter(resultPreview, e.getGlobal());
+}
+
+function addNumber(key, resultPreview) {
+    if (!e.getSymbol()) {
+        e.setPrev(e.getGlobal());
+        e.setGlobal(e.getGlobal() + key.textContent);
+        e.setSymbol('add');
+        displayKeyCharacter(resultPreview, e.getGlobal());
+    } else {
+        let next = e.getGlobal().split('+')[1];
+        e.setNext(next);
+        let result = count(e.getPrev(), e.getNext(), e.getSymbol());
+        e.setResult(result);
+        
+        e.setSymbol('');
+        e.setGlobal(e.getResult());
+        displayKeyCharacter(resultPreview, e.getResult());
+    }
+}
+
+function divisionNumber(key, resultPreview) {
+    console.log('----');
+}
+
+function subtractNumber(key, resultPreview) {
+    console.log('-1--');
+}
+
+function cleanNumber(key, resultPreview) {
+    e.setGlobal('0');
+    e.setNext(0);
+    e.setPrev(0);
+    e.setSymbol('');
+    e.setResult(0);
+    displayKeyCharacter(resultPreview, e.getGlobal());
+}
+
+function equalNumber(key, resultPreview) {
+    if (e.getSymbol() === 'add') {
+        addNumber(key, resultPreview);
+    }
+}
--- test/other/test.js
+++ test/other/test.js
@@ -1,34 +1,71 @@
-function checkNumber(character) {
-    let tmp = '11';
-    if (!character) {
-        throw Error('不是数字');
-    }
-    if (typeof str === 'number') {
-        character = String(character);
-    }
-    switch (character) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            tmp = character;
-            break;
-        case '÷':
-        case 'x':
-        case '-':
-        case '+':
-            tmp = 'calculator';
-            break;
-        default:
-            return '暂不支持';
-    }
-    return tmp;
-}
+'use strict'
+/* let globalNumber = '';
+let prevNumber = 0;
+let nextNumber = 0;
+let result = 0; */
 
-console.log(checkNumber('-'));
+const myNumber = (function() {
+    // 使用Symbol生成唯一指定标识
+    let _globalNumber = Symbol('globalNumber');
+    let _prevNumber = Symbol('prevNumber');
+    let _nextNumber = Symbol('nextNumber');
+    let _result = Symbol('result');
+    let _symbol = Symbol('symbol');
+
+    class myNumber {
+        constructor() {
+            this[_globalNumber] = '1';
+            this[_prevNumber] = 0;
+            this[_nextNumber] = 0;
+            this[_result] = 0;
+            this[_symbol] = '';
+        }
+
+        getGlobal() {
+            return this[_globalNumber];
+        }
+
+        setGlobal(newGlobal = 'none') {
+            this[_globalNumber] = newGlobal;
+        }
+        
+        getPrev() {
+            return this[_prevNumber];
+        }
+
+        setPrev(newPrev = 'none') {
+            this[_prevNumber] = newPrev;
+        }
+
+        getNext() {
+            return this[_nextNumber];
+        }
+
+        setNext(newNext = 'none') {
+            this[_nextNumber] = newNext;
+        }
+
+        getResult() {
+            return this[_result];
+        }
+
+        setResult(newResult = 'none') {
+            this[_result] = newResult;
+        }
+
+        getSymbol() {
+            return this[_symbol];
+        }
+
+        setSymbol(newSymbol = 'none') {
+            this[_symbol] = newSymbol;
+        }
+    }
+
+    return myNumber;
+})();
+
+
+let e = new myNumber();
+console.log(e.setNext(2));
+console.log(e.getNext());
